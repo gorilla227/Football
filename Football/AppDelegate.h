4b95570a9892f8ad5ea46bebfa9c77c1c06ce138
Andy <Andy@AndydeMacBook-Pro.local>
2014-03-08 19:58:03 -0800

    Initial Commit

---
+++ Football/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Football
+//
+//  Created by Andy on 14-3-8.
+//  Copyright (c) 2014年 Xinyi Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Football/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Football
+//
+//  Created by Andy on 14-3-8.
+//  Copyright (c) 2014年 Xinyi Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
