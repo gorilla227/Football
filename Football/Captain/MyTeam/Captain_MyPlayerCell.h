ce2608ea705dacb48cb7b9f9d8c1517c56f022ac
Andy <Andy@AndydeMacBook-Pro.local>
2014-04-06 14:01:19 -0700

    Restructure Captain folder.

---
+++ Football/Captain/MyTeam/Captain_MyPlayerCell.h
@@ -0,0 +1,13 @@
+//
+//  Captain_MyPlayerCell.h
+//  Football
+//
+//  Created by Andy on 14-4-6.
+//  Copyright (c) 2014年 Xinyi Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Captain_MyPlayerCell : UITableViewCell
+
+@end
---
+++ Football/Captain/MyTeam/Captain_MyPlayerCell.m
@@ -0,0 +1,34 @@
+//
+//  Captain_MyPlayerCell.m
+//  Football
+//
+//  Created by Andy on 14-4-6.
+//  Copyright (c) 2014年 Xinyi Xu. All rights reserved.
+//
+
+#import "Captain_MyPlayerCell.h"
+
+@implementation Captain_MyPlayerCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
