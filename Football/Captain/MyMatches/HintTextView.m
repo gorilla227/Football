7c0afe552c038c3893e7f5d2513b221ddbe0116a
gorilla227 <gorilla.andy@gmail.com>
2014-04-26 14:15:31 -0700

    Update HintView init method.

--- Football/Captain/MyMatches/HintTextView.m
+++ Football/Captain/MyMatches/HintTextView.m
@@ -7,7 +7,9 @@
 //
 #import "HintTextView.h"
 
-@implementation HintTextView
+@implementation HintTextView{
+    UIImage *arrow;
+}
 @synthesize boundedView;
 
 - (id)initWithFrame:(CGRect)frame
@@ -15,6 +17,33 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+    }
+    return self;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        //Get arrow
+        arrow = [UIImage imageNamed:@"hint_background_arrow.png"];
+        UIImageView *arrowView = [[UIImageView alloc] initWithImage:arrow];
+        [arrowView setAlpha:0.5f];
+        [self.layer setMasksToBounds:NO];
+        CGRect arrowFrame = arrowView.frame;
+        arrowFrame.origin.x = 20;
+        arrowFrame.origin.y = -arrow.size.height;
+        [arrowView setFrame:arrowFrame];
+        [self addSubview:arrowView];
+        
+        //Set backgroundcolor and cornerRadius
+        [self setBackgroundColor:[UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.5f]];
+        [self.layer setCornerRadius:5.0f];
+        
+        //Set attributes
+        [self setSelectable:NO];
+        [self setEditable:NO];
+        [self setScrollEnabled:NO];
     }
     return self;
 }
@@ -37,16 +66,6 @@
     [self setTextColor:[UIColor whiteColor]];
     [self setFont:[UIFont fontWithName:self.font.fontName size:14]];
     
-    //Get arrow
-    UIImage *arrow = [UIImage imageNamed:@"hint_background_arrow.png"];
-    UIImageView *arrowView = [[UIImageView alloc] initWithImage:arrow];
-    [arrowView setAlpha:0.5f];
-    [self.layer setMasksToBounds:NO];
-    CGRect arrowFrame = arrowView.frame;
-    arrowFrame.origin.x = 20;
-    arrowFrame.origin.y = -arrow.size.height;
-    [arrowView setFrame:arrowFrame];
-    
     //Get frame
     CGRect frame = dockView.frame;
     frame.origin.x += 5;
@@ -54,17 +73,7 @@
     frame.size.width = 320 - frame.origin.x * 2;
     frame.size.height = [self sizeThatFits:CGSizeMake(frame.size.width, FLT_MAX)].height;
     [self setFrame:frame];
-    [self addSubview:arrowView];
-    
-    //Set backgroundcolor and cornerRadius
-    [self setBackgroundColor:[UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.5f]];
-    [self.layer setCornerRadius:5.0f];
-    
-    //Set attributes
-    [self setSelectable:NO];
-    [self setEditable:NO];
-    [self setScrollEnabled:NO];
-    
+
     //Set the boundedView
     boundedView = dockView;
     
