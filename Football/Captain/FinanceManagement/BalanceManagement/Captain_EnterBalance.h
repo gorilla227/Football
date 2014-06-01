89352586fe9d96556682807f42b2143f8fa28a96
gorilla227 <gorilla.andy@gmail.com>
2014-06-01 00:00:57 -0700

    Adjust the file structure.

---
+++ Football/Captain/FinanceManagement/BalanceManagement/Captain_EnterBalance.h
@@ -0,0 +1,13 @@
+//
+//  Captain_EnterBalance.h
+//  Football
+//
+//  Created by Andy on 14-5-31.
+//  Copyright (c) 2014年 Xinyi Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Captain_EnterBalance : UIViewController
+
+@end
