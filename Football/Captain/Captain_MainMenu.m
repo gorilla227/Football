d7b0a3ee1ce569b5fd107544613ed1765d18377d
gorilla227 <gorilla.andy@gmail.com>
2014-07-06 23:18:04 -0700

    Update the Menu: Add myProfile section.

--- Football/Captain/Captain_MainMenu.m
+++ Football/Captain/Captain_MainMenu.m
@@ -18,6 +18,7 @@
     UIFont *unselectedFont;
     NSString *menuCellIdentifier_root;
     NSString *menuCellIdentifier_lesser;
+    NSString *menuCellIdentifier_myProfile;
     NSInteger lastRootMenuIndex;
     NSIndexPath *visibleViewIndexPath;
 }
@@ -42,6 +43,7 @@
     //Set the static string for cell identifiers
     menuCellIdentifier_root = @"RootMenu";
     menuCellIdentifier_lesser = @"LesserMenu";
+    menuCellIdentifier_myProfile = @"MyProfile";
     
     //Set the font for selected/unselected menu cell.
     UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:menuCellIdentifier_lesser];
@@ -52,13 +54,13 @@
     //Set the tableheaderview and tablefooterview
     CGRect headerFrame = self.tableView.tableHeaderView.frame;
 //    headerFrame.size.height = self.tableView.sectionFooterHeight;
-    headerFrame.size.height = 0.1;
+    headerFrame.size.height = 5.0f;
     [self.tableView setTableHeaderView:[[UIView alloc] initWithFrame:headerFrame]];
 
     //Generate the menulist
     [self menuListGeneration];
-    lastRootMenuIndex = 0;//Unfolder root menu index
-    visibleViewIndexPath = [NSIndexPath indexPathForRow:1 inSection:0];//Select lessor menu item index
+    lastRootMenuIndex = 1;//Unfolder root menu index
+    visibleViewIndexPath = [NSIndexPath indexPathForRow:1 inSection:1];//Select lessor menu item index
     
     //Set background
     UIImage *backgroundImage = [UIImage imageNamed:@"menu_bg@2x.png"];
@@ -94,74 +96,93 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return [[menuListDictionary objectForKey:@"RootMenu"] count];
+    return [[menuListDictionary objectForKey:@"RootMenu"] count] + 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return [[menuListDictionary objectForKey:[NSString stringWithFormat:@"%li", section]] count] + 1;
+    if (section == 0) {
+        return 1;
+    }
+    else {
+        return [[menuListDictionary objectForKey:[NSString stringWithFormat:@"%li", section - 1]] count] + 1;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell;
-
-    if (indexPath.row == 0) {
-        //Root Menu Item
-        cell = [tableView dequeueReusableCellWithIdentifier:menuCellIdentifier_root];
-        [cell.textLabel setText:[[[menuListDictionary objectForKey:@"RootMenu"] objectAtIndex:indexPath.section] objectForKey:@"Title"]];
-        [cell.imageView setImage:[UIImage imageNamed:[[[menuListDictionary objectForKey:@"RootMenu"] objectAtIndex:indexPath.section] objectForKey:@"Icon"]]];
-        [cell.imageView setContentMode:UIViewContentModeScaleToFill];
-//        [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
-//        [cell setBackgroundColor:[UIColor grayColor]];
+    if (indexPath.section == 0) {
+        cell = [tableView dequeueReusableCellWithIdentifier:menuCellIdentifier_myProfile];
+        [cell.imageView setImage:gMyUserInfo.playerPortrait];
+        [cell.imageView.layer setBorderColor:[UIColor whiteColor].CGColor];
+        [cell.imageView.layer setBorderWidth:2.0f];
+        [cell.imageView.layer setCornerRadius:5.0f];
+        [cell.imageView.layer setMasksToBounds:YES];
+        [cell.textLabel setText:gMyUserInfo.nickName];
+        [cell.detailTextLabel setText:[[gUIStrings objectForKey:@"UI_Positions"] objectAtIndex:gMyUserInfo.position]];
     }
     else {
-        NSDictionary *menuItem = [[menuListDictionary objectForKey:[NSString stringWithFormat:@"%li", indexPath.section]] objectAtIndex:indexPath.row - 1];
-        
-        //Lesser Menu Item
-        cell = [tableView dequeueReusableCellWithIdentifier:menuCellIdentifier_lesser];
-        [cell.textLabel setText:[menuItem objectForKey:@"Title"]];
-//        [cell setAccessoryType:UITableViewCellAccessoryNone];
-        [self formatCell:cell withFont:unselectedFont];
-        [cell.imageView setAlpha:0];
+        if (indexPath.row == 0) {
+            //Root Menu Item
+            cell = [tableView dequeueReusableCellWithIdentifier:menuCellIdentifier_root];
+            [cell.textLabel setText:[[[menuListDictionary objectForKey:@"RootMenu"] objectAtIndex:indexPath.section - 1] objectForKey:@"Title"]];
+            [cell.imageView setImage:[UIImage imageNamed:[[[menuListDictionary objectForKey:@"RootMenu"] objectAtIndex:indexPath.section - 1] objectForKey:@"Icon"]]];
+            [cell.imageView setContentMode:UIViewContentModeScaleToFill];
+        }
+        else {
+            NSDictionary *menuItem = [[menuListDictionary objectForKey:[NSString stringWithFormat:@"%li", indexPath.section - 1]] objectAtIndex:indexPath.row - 1];
+            
+            //Lesser Menu Item
+            cell = [tableView dequeueReusableCellWithIdentifier:menuCellIdentifier_lesser];
+            [cell.textLabel setText:[menuItem objectForKey:@"Title"]];
+            [self formatCell:cell withFont:unselectedFont];
+            [cell.imageView setAlpha:0];
+        }
     }
-//    [cell setUserInteractionEnabled:(indexPath.row != 0)];
     return cell;
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-    
-    if (indexPath.row == 0) {
-        lastRootMenuIndex = indexPath.section;
-        [self.tableView reloadData];
-        [self viewWillAppear:NO];
+    if (indexPath.section == 0) {
+        //Turn to MyProfileView
+        NSLog(@"MyProfile");
     }
     else {
-        //Format the cell
-        [self formatCell:cell withFont:selectedFont];
-        
-        //Call the parentcontroller to switch lesser view
-        NSDictionary *menuItem = [[menuListDictionary objectForKey:[NSString stringWithFormat:@"%li", indexPath.section]] objectAtIndex:indexPath.row - 1];
-        NSString *selectedView = [menuItem objectForKey:@"Identifier"];
-        if (selectedView.length != 0) {
-            [delegateOfViewSwitch switchSelectMenuView:selectedView];
+        if (indexPath.row == 0) {
+            lastRootMenuIndex = indexPath.section;
+            [self.tableView reloadData];
+            [self viewWillAppear:NO];
         }
-        NSLog(@"%@", [menuItem objectForKey:@"Title"]);
+        else {
+            //Format the cell
+            [self formatCell:cell withFont:selectedFont];
+            
+            //Call the parentcontroller to switch lesser view
+            NSDictionary *menuItem = [[menuListDictionary objectForKey:[NSString stringWithFormat:@"%li", indexPath.section - 1]] objectAtIndex:indexPath.row - 1];
+            NSString *selectedView = [menuItem objectForKey:@"Identifier"];
+            if (selectedView.length != 0) {
+                [delegateOfViewSwitch switchSelectMenuView:selectedView];
+            }
+            NSLog(@"%@", [menuItem objectForKey:@"Title"]);
 
-        //Close the menu
-        [delegateOfMenuAppearance menuSwitch];
-        
-        visibleViewIndexPath = indexPath;
+            //Close the menu
+            [delegateOfMenuAppearance menuSwitch];
+            
+            visibleViewIndexPath = indexPath;
+        }
     }
 }
 
 -(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-    [self formatCell:cell withFont:unselectedFont];
+    if (indexPath.section != 0) {
+        UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+        [self formatCell:cell withFont:unselectedFont];
+    }
 }
 
 -(void)menuListGeneration
